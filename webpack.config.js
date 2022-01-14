5368ac79c1b79a588391023fc24dbf088636994a
Артем Лобовский <alobovskiy@ad.local>
2022-01-14 15:48:25 +0300

    html+css compile with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,7 +16,9 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'style.css',
         }),
-        
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+      })
   ],
 
   module: {
