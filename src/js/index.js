0f247352e26810d763b6aabb0c7f9a50d9941a70
Артем Лобовский <alobovskiy@ad.local>
2022-01-14 15:30:08 +0300

    css with webpack

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/style.css';
+import '../font/fontello/css/animation.css';
+import '../font/fontello/css/fontello-codes.css';
+import '../font/fontello/css/fontello-embedded.css';
+import '../font/fontello/css/fontello-ie7-codes.css';
+import '../font/fontello/css/fontello-ie7.css';
+import '../font/fontello/css/fontello.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'eval-source-map',
+  entry: './src/js/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
-  watch: true
+  watch: true,
+
+  plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        
+      },
+    ],
+  },
 };
