39f9cecae6b541f8f8803aa9614998c0f1f13a6d
Артем Лобовский <alobovskiy@ad.local>
2022-01-18 08:40:50 +0300

    with CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/index.js',
+  entry: './src/index.js',
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -13,65 +14,55 @@ module.exports = {
   },
   watch: false,
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'src/favicon.png', to: 'favicon.png' },
-      ],
-    }),
-  ],
-
   module: {
     rules: [
       {
         test: /\.html/,
-        loader: 'html-loader',
-        options: {
-          sources: {
-            list: [
-              {
-                tag: "img",
-                attribute: "src",
-                type: "src",
-              },
-              {
-                // Tag name
-                tag: "link",
-                // Attribute name
-                attribute: "href",
-                // Type of processing, can be `src` or `scrset`
-                type: "src",
-                // Allow to filter some attributes
-                filter: (tag, attribute, attributes, resourcePath) => {
-                  // The `tag` argument contains a name of the HTML tag.
-                  // The `attribute` argument contains a name of the HTML attribute.
-                  // The `attributes` argument contains all attributes of the tag.
-                  // The `resourcePath` argument contains a path to the loaded HTML file.
+        use: [
+          
+          {
+            loader: 'html-loader',
+            options: {
+              sources: {
+                list: [
+                  {
+                    tag: "img",
+                    attribute: "src",
+                    type: "src",
+                  },
+                  {
+                    // Tag name
+                    tag: "link",
+                    // Attribute name
+                    attribute: "href",
+                    // Type of processing, can be `src` or `scrset`
+                    type: "src",
+                    // Allow to filter some attributes
+                    filter: (tag, attribute, attributes, resourcePath) => {
+                      // The `tag` argument contains a name of the HTML tag.
+                      // The `attribute` argument contains a name of the HTML attribute.
+                      // The `attributes` argument contains all attributes of the tag.
+                      // The `resourcePath` argument contains a path to the loaded HTML file.
 
-                  if (!/stylesheet/i.test(attributes.rel)) {
-                    return false;
-                  }
+                      if (!/stylesheet/i.test(attributes.rel)) {
+                        return false;
+                      }
 
-                  if (
-                    attributes.type &&
-                    attributes.type.trim().toLowerCase() !== "text/css"
-                  ) {
-                    return false;
-                  }
+                      if (
+                        attributes.type &&
+                        attributes.type.trim().toLowerCase() !== "text/css"
+                      ) {
+                        return false;
+                      }
 
-                  return true;
-                },
-              },
-            ]
+                      return true;
+                    },
+                  },
+                ]
+              }
+            }
           }
-        }
+        ]
       },
       {
         test: /\.s[ca]ss$/,
@@ -96,7 +87,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'assets/font/[hash][ext]'
@@ -104,4 +95,22 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'fusely template',
+      favicon: './src/favicon.png',
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'src/favicon.png', to: 'favicon.png' },
+    //   ],
+    // }),
+  ]
 };
